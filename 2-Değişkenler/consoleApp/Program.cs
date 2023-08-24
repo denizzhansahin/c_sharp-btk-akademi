126471afdb9d14e198599c41c80b329989413600
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-08-24 14:38:34 +0300

    Add files via upload

---
+++ 2-Değişkenler/consoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿var kdvOrani = 1.18; //değişken oluşturma
+var urunA = 5000;
+var urunB = 6000;
+var urunC = 7000;
+var urunD = 5000;
+
+Console.WriteLine("Eski Alan");
+Console.WriteLine(5000 * 1.18); //urunA
+Console.WriteLine(6000 * 1.18); //urunB
+Console.WriteLine(7000 * kdvOrani); //urunC
+Console.WriteLine(5000 * kdvOrani); //urunD
+
+Console.WriteLine("Yeni Alan");
+Console.WriteLine(urunA * kdvOrani); //urunA
+Console.WriteLine(urunB * kdvOrani); //urunB
+Console.WriteLine(urunC * kdvOrani); //urunC
+Console.WriteLine(urunD * kdvOrani); //urunD
