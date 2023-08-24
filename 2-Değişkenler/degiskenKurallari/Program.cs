e974925a1a5d361ed5f6e823138be32c27096a79
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-08-24 14:37:37 +0300

    Add files via upload

---
+++ 2-Değişkenler/degiskenKurallari/Program.cs
@@ -0,0 +1,28 @@
+﻿var sayi = 20;
+var urunAdi = "Samsung S23";
+var urun_adi = "Samsung S23 Deneme";
+var sayi2sayi = 12312;
+var fiyat = 1000.25;
+var Fiyat = 1000.25;
+var satisMi = true;
+var satisMi2 = false;
+var ö = 213234;
+
+//yorum satırı
+//var yok = 30;
+
+
+Console.WriteLine(sayi);
+Console.WriteLine(urunAdi);
+Console.WriteLine(urun_adi);
+Console.WriteLine(sayi2sayi);
+
+sayi2sayi = 111111;
+Console.WriteLine(sayi2sayi);
+
+Console.WriteLine(fiyat);
+Console.WriteLine(Fiyat);
+Console.WriteLine(satisMi);
+Console.WriteLine(satisMi2);
+
+Console.WriteLine(ö);
---
+++ 2-Değişkenler/degiskenUygulama/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Merhaba burası bir yorum satırı alanıdır
+Nerdesiniz?
+Bu kod burada!!!!!!!!!!!!!!!!!!!!
+*/
+
+
+// q
+// q
+// q
+// q
+// q
+
+var ogrenciAdi = "Denizhan";
+var ogrenciSoyadi = "Şahin";
+var adSoyad = "Denizhan Şahin";
+var adSoyad2 = ogrenciAdi + " " + ogrenciSoyadi;
+var adSoyad3 = ogrenciAdi + ogrenciSoyadi;
+var ogrenciNo = "10000";
+var ogrenciNo2 = 10000;
+var ogrenciDogumYili = 2005;
+var Yil = 2023;
+var ogrenciYas = Yil - ogrenciDogumYili;
+var ogrenciAdres = "Yozgat";
+
+
+Console.WriteLine(ogrenciAdi);
+Console.WriteLine(ogrenciSoyadi);
+Console.WriteLine(adSoyad);
+Console.WriteLine(adSoyad2);
+Console.WriteLine(adSoyad3);
+Console.WriteLine(ogrenciNo);
+Console.WriteLine(ogrenciNo2);
+Console.WriteLine(ogrenciDogumYili);
+Console.WriteLine(ogrenciYas);
+Console.WriteLine(ogrenciAdres);
+
+////////////////////////////////////////////////
+///
+
+var urun1_fiyat = 50;
+var urun2_fiyat = 60.5;
+var urun3_fiyat = 356.45;
+
+var toplam = urun1_fiyat + urun2_fiyat + urun3_fiyat;
+Console.WriteLine(toplam);
