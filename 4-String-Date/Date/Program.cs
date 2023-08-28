98dbc516e1d6f780fc436b015f738ebdf39f291b
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-08-28 17:25:26 +0300

    Add files via upload

---
+++ 4-String-Date/Date/Program.cs
@@ -0,0 +1,27 @@
+﻿var simdi = DateTime.Now;
+DateTime simdi2 = DateTime.Now;
+
+Console.WriteLine("SİMDİ " + simdi);
+Console.WriteLine("SİMDİ2 " + simdi2);
+
+//metodlar
+Console.WriteLine(simdi.Year);
+Console.WriteLine(simdi.Month);
+Console.WriteLine(simdi.Day);
+Console.WriteLine(simdi.DayOfWeek);
+Console.WriteLine(simdi.Hour);
+Console.WriteLine(simdi.Minute);
+Console.WriteLine(simdi.Second);
+Console.WriteLine(simdi.Millisecond);
+
+//olusturma
+DateTime dt = new DateTime(2018,6,10,12,15,13,14);
+Console.WriteLine(dt);
+DateTime dt2 = dt.AddYears(1);;
+DateTime dt3 = dt.AddHours(1);;
+Console.WriteLine(dt2);
+Console.WriteLine(dt3);
+
+var fark = simdi - dt;
+Console.WriteLine(fark.TotalDays);
+Console.WriteLine(fark.TotalHours);
---
+++ 4-String-Date/String/Program.cs
@@ -0,0 +1,20 @@
+﻿char cinsiyet = 'E';
+string cinsiyet2 = "Erkek";
+string cinsiyet3 = "E";
+
+string? ad = Console.ReadLine();
+string? soyad = Console.ReadLine();
+string? yas = Console.ReadLine();
+
+int dogumYili = 2000;
+string mesaj = "Ahmet turan isimli kişi 20 yaşındadır";
+//string concat
+string mesaj1 = ad +" "+  soyad +" "+ "isimli kişi 20 yaşındadır";
+
+Console.WriteLine(mesaj);
+Console.WriteLine(mesaj1);
+
+
+//string interpolation
+string mesaj3 = $"{ad} {soyad} kişisi {yas} yaşındadır.";
+Console.WriteLine(mesaj3);
