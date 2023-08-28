93fa8d1314df4d85ca567175c372f6f5964455e8
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-08-28 17:26:35 +0300

    Add files via upload

---
+++ 4-String-Date/String-Uygulama/Program.cs
@@ -0,0 +1,26 @@
+﻿string kursAdi = ".NET 7 ile C# Programlama Dili";
+//1 kaç karakter ve
+//2 hepsini kucuk karakter yap
+//3 '.' ile mi baslamaktadir
+//4 'C#' bilgisi hangi konumdadır?
+//5 string 'C#' bilgisi iceriyormu
+//6 'Dili' kelimesi yerine 'Dersleri' yaziniz. (Replace)
+
+//1
+var karakterSayisi = kursAdi.Length;
+Console.WriteLine(karakterSayisi);
+//2
+var kucukHarf = kursAdi.ToLower();
+Console.WriteLine(kucukHarf);
+//3
+var nokta = kursAdi.StartsWith('.');
+Console.WriteLine("string . ile başlıyor : " + nokta);
+//4
+var konum = kursAdi.IndexOf("C#");
+Console.WriteLine("konum : " + konum);
+//5
+var iceriyormu = kursAdi.Contains("C#");
+Console.WriteLine($"C# içeriyor mu : {iceriyormu}");
+//6
+var guncellenmisString = kursAdi.Replace("Dili","Dersleri");
+Console.WriteLine(guncellenmisString);
---
+++ 4-String-Date/StringMetodlari/Program.cs
@@ -0,0 +1,45 @@
+﻿string mesaj = " Denizhan isimli kişi 20 yaşındadır. ";
+Console.WriteLine(mesaj);
+Console.WriteLine(mesaj.Length.ToString());
+
+var sonuc = mesaj.ToLower(); // hepsi küçük karakter oldu
+Console.WriteLine(sonuc);
+
+var sonuc1 = mesaj.ToUpper(); // hepsi büyük olsun
+Console.WriteLine(sonuc1);
+
+var sonuc2 = mesaj.Trim();//baştaki sondaki boşlukları sil
+Console.WriteLine(sonuc2);
+
+var sonuc3 = mesaj.Split(" "); // bir karaktere göre string dizisine ayır
+Console.WriteLine(sonuc3);
+
+var sonuc4 = mesaj[3]; // string içindeki index'e denk gelen değeri gör
+Console.WriteLine(sonuc4);
+
+var sonuc5 = mesaj.Split(" ")[3]; // bir karaktere göre string dizisine ayır
+Console.WriteLine(sonuc5);
+
+string mesaj1 = "Denizhan isimli kişi 20 yaşındadır.";
+var sonuc6 = mesaj1.StartsWith("Denizhan"); //bu ile mi başladı
+Console.WriteLine(sonuc6);
+var sonuc7 = mesaj1.EndsWith("."); //bu ile mi bitti
+Console.WriteLine(sonuc7);
+
+var sonuc8 = mesaj1.Contains("Denizha"); //arama, var mı yok mu
+Console.WriteLine(sonuc8);
+
+var sonuc9 = mesaj1.Contains("Mehmet"); //arama, var mı yok mu
+Console.WriteLine(sonuc9);
+
+var sonuc10 = mesaj1.IndexOf("kişi"); //kaçıncı yerde var
+Console.WriteLine(sonuc10);
+
+var sonuc11 = mesaj1.IndexOf("addd"); //kaçıncı yerde var, -1 ise yok demektir
+Console.WriteLine(sonuc11);
+
+var sonuc12 = mesaj1.Substring(10);//bu stringi getir
+Console.WriteLine(sonuc12);
+
+var sonuc13 = mesaj1.Substring(0,15);//bu stringi getir
+Console.WriteLine(sonuc13);
