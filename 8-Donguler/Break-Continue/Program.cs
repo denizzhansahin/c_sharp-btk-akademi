197ecc17d705430d97366d475102e6bd810e27d2
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 16:47:57 +0300

    Add files via upload

---
+++ 8-Donguler/Break-Continue/Program.cs
@@ -0,0 +1,17 @@
+﻿string isim = "Denizhan ŞAHİN";
+
+for(var i = 0; i<isim.Length;i++){
+    if(isim[i]=='H'){
+        break;
+    }
+    Console.WriteLine(isim[i]);
+}
+
+int x = 0;
+while(x<5){
+    x++;
+    if(x==3){
+        continue;
+    }
+    Console.WriteLine(x);
+}
---
+++ 8-Donguler/DoWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿int i = 1;
+do{
+    Console.WriteLine(i);
+    i++;
+}while(i<=10);
+
+
+
+
+Console.Write("adet : ");
+int adet = Convert.ToInt32(Console.ReadLine());
+
+string[] urunler = new string[adet];
+
+int i1 = 0;
+
+do{
+    Console.Write("Urun adi : ");
+    urunler[i1] = Console.ReadLine() ?? "";
+    i1++;
+}while(adet != i1);
+
+Console.WriteLine("ürünler listeleniyor.");
+
+for(var a = 0; a<urunler.Length;a++){
+    Console.WriteLine(urunler[a]);
+}
