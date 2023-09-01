fa01c1cb23d154e1fd8744a72f7701d134b9d870
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 16:52:53 +0300

    Add files via upload

---
+++ 9-Dosyalar/DosyaOkuma/Program.cs
@@ -0,0 +1,16 @@
+﻿StreamReader sr = File.OpenText("deneme.txt");
+var s = "";
+// Console.WriteLine(sr.ReadLine());
+// Console.WriteLine(sr.ReadLine());
+
+while((s = sr.ReadLine()) != null){
+    Console.WriteLine(s);
+}
+
+
+string sonuc = File.ReadAllText("deneme.txt");
+Console.WriteLine(sonuc);
+
+string[] sonuc1 = File.ReadAllLines("deneme.txt");
+Console.WriteLine(sonuc1[0]);
+Console.WriteLine(sonuc1[1]);
---
+++ 9-Dosyalar/DosyaVeKlasor/Program.cs
@@ -0,0 +1,57 @@
+﻿string rootpath  = Directory.GetCurrentDirectory();
+Console.WriteLine(rootpath);
+
+//klasörler
+string[] dirs = Directory.GetDirectories(rootpath,"*",SearchOption.TopDirectoryOnly);
+
+foreach(var dir in dirs){
+    Console.WriteLine(dir);
+}
+
+//dosyalar
+string[] files = Directory.GetFiles(rootpath,"*",SearchOption.AllDirectories);
+
+foreach(var file in files){
+    Console.WriteLine(file);
+}
+
+//dosyalar
+string[] files1 = Directory.GetFiles(rootpath,"*.txt",SearchOption.AllDirectories);
+
+foreach(var file1 in files1){
+    Console.WriteLine(file1);
+    Console.WriteLine(Path.GetExtension(file1));//uzantı
+    Console.WriteLine(Path.GetFileNameWithoutExtension(file1));//uzantı olmadan dosya adı
+    Console.WriteLine(Path.GetFileName(file1));//dosya bilgisi
+
+    var info  = new FileInfo(file1);
+    Console.WriteLine($"{Path.GetFileName(file1)} : {info.Length}");
+
+}
+
+
+//dosyalar
+string source_path  = Directory.GetCurrentDirectory() + "/img";
+string dest_path  = Directory.GetCurrentDirectory() + "/images/";
+
+string[] files2 = Directory.GetFiles(source_path,"*",SearchOption.AllDirectories);
+
+foreach(var file2 in files2){
+    Console.WriteLine(file2);
+    Console.WriteLine(Path.GetExtension(file2));//uzantı
+    Console.WriteLine(Path.GetFileNameWithoutExtension(file2));//uzantı olmadan dosya adı
+    Console.WriteLine(Path.GetFileName(file2));//dosya bilgisi
+
+    var info  = new FileInfo(file2);
+    Console.WriteLine($"{Path.GetFileName(file2)} : {info.Length}");
+
+    if(!Directory.Exists(dest_path)){
+        Directory.CreateDirectory(dest_path);
+    }
+
+    //File.Copy(file2,$"{dest_path}{Path.GetFileName(file2)}"); //dosya, isim seç
+
+    string name = Path.GetRandomFileName() + Path.GetExtension(file2); // rastgele isim üretmek
+    File.Copy(file2,$"{dest_path}{name}"); //dosya, isim seç
+
+}
