c245dbf094ea673363194c0832e44a8a47636897
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 16:41:28 +0300

    Add files via upload

---
+++ 7-KosulluBloklar/IF-ELSE/Program.cs
@@ -0,0 +1,31 @@
+﻿string username = "deniz";
+string password = "1234";
+
+if(username == "deniz"){
+    if(password == "1234"){
+        Console.WriteLine("Merhaba Denizhan");
+    }else{
+        Console.WriteLine("parola yanlış");
+    }
+}else{
+    Console.WriteLine("username yanlış");
+}
+
+if(username!="deniz"){
+    Console.WriteLine("username yanlış");
+}else if(password!="1234"){
+    Console.WriteLine("parola yanlış");
+}else{
+    Console.WriteLine("Merhaba");
+}
+
+int x = 20;
+int y = 20;
+
+if(x>y){
+    Console.WriteLine("x y'den büyük");
+}else if(x==y){
+    Console.WriteLine("x y'e eşit");
+}else{
+    Console.WriteLine("x y'den büyük");
+}
