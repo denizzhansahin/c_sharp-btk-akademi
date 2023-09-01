2b84b67eab1c9b3b2e81d75c805cf6cd5ebead68
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 16:44:07 +0300

    Add files via upload

---
+++ 7-KosulluBloklar/ternary/Program.cs
@@ -0,0 +1,16 @@
+﻿int sayi = 10;
+
+var sonuc = (sayi % 2== 0) ? "sayı çift" : "sayi tek";
+Console.WriteLine(sonuc);
+
+sonuc = (sayi % 2== 0) ? 
+    "sayi çift" :
+     "sayi  tek";
+
+Console.WriteLine(sonuc);
+
+
+sonuc = (sayi % 2 == 0) ?
+    (sayi>0) ? "sayı pozitif çift" : "sayı negatif çift":
+    (sayi>0) ? "sayı pozitif tek" : "sayi negatif tek";
+Console.WriteLine(sonuc);
