b1ffbbd313b93b63251ceda5c09a710c8c3af1ec
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 16:54:16 +0300

    Add files via upload

---
+++ 9-Dosyalar/DosyaYazmaEkleme/Program.cs
@@ -0,0 +1,32 @@
+﻿StreamWriter sw = File.CreateText("deneme.txt");
+sw.WriteLine("Merhaba");
+sw.WriteLine("BTK");
+sw.WriteLine("Akademi");
+
+sw.Close();
+
+
+using (StreamWriter sw1 = File.CreateText("deneme.txt")){
+    sw1.WriteLine("Merhaba");
+    sw1.WriteLine("BTK");
+    sw1.WriteLine("Akademi1");
+}
+
+
+using (StreamWriter sw1 = File.AppendText("deneme.txt")){
+    sw1.WriteLine("Merhaba");
+    sw1.WriteLine("BTK");
+    sw1.WriteLine("Akademi1111111");
+}
+
+using(StreamReader sr = File.OpenText("deneme.txt")){
+    var s = "";
+    while((s=sr.ReadLine())!=null){
+        Console.WriteLine(s);
+    }
+}
+
+
+File.WriteAllText("deneme2.txt","merhaba");
+File.WriteAllText("deneme2.txt","merhaba1");
+File.AppendAllText("deneme2.txt","merhaba1");
---
+++ 9-Dosyalar/KlasorlerCalismak/Program.cs
@@ -0,0 +1,21 @@
+﻿Directory.CreateDirectory("temp");
+Directory.CreateDirectory("temp/deneme");
+Directory.CreateDirectory("temp/deneme2");
+
+if(Directory.Exists("temp/deneme2")){
+    Directory.Delete("temp/deneme2");
+}else{
+    Console.WriteLine("silmek istenen klasör yok");
+}
+
+Directory.CreateDirectory("temp/deneme2");
+//Directory.Move("temp/deneme2", "temp/deneme");
+
+//string path = "/home/denshn/Masaüstü/C#-BTK/9-Dosyalar/KlasorlerCalismak/temp/deneme3";
+//Directory.CreateDirectory(path);
+
+string path  = Directory.GetCurrentDirectory();
+Console.WriteLine(path);
+
+path  = Directory.GetCurrentDirectory() + "/temp/deneme4";
+Directory.CreateDirectory(path);
