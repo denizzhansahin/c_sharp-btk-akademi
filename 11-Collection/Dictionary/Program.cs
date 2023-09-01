c9d79374812e57657c719a767b44fbcbff4a120e
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 17:07:31 +0300

    Add files via upload

---
+++ 11-Collection/Dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp{
+    class Program{
+        static void Main(string[] args){
+            //dictionary
+            //key - value
+            Dictionary<int,string> plakalar = new Dictionary<int,string>();
+            plakalar.Add(41,"Kocaeli");
+            plakalar.Add(34,"İstanbul");
+            plakalar.Add(53,"Rize");
+
+            Dictionary<int,string> sayilar = new Dictionary<int, string>(){
+                {1,"Bir"},{2,"İki"},{3,"Üç"}
+            };
+            Console.WriteLine(plakalar[41]);
+
+            if(plakalar.ContainsKey(34)){
+                Console.WriteLine(plakalar[34]);
+            }
+
+            foreach(KeyValuePair<int,string> plaka in plakalar){
+                Console.WriteLine(plaka.Key + " " + plaka.Value);
+            }
+
+            //update
+            sayilar[1] = "one";
+            //sayilar.Remove(1);
+            //sayilar.Clear();
+
+            Console.WriteLine(sayilar[1]);
+
+
+        }
+    }
+}
---
+++ 11-Collection/GenericList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace GenericList{
+    class Program{
+        static void Main(string[] args){
+            //generic list
+            List<int> sayilar = new List<int>();
+            sayilar.Add(10);
+            sayilar.Add(20);
+
+            List<string> isimler = new List<string>() {"ali","ahmet","ayşe"};
+            List<Product> urunler = new List<Product>();
+            urunler.Add(new Product(){Id=1,Title="Iphone 14",Price=40000});
+            urunler.Add(new Product(){Id=2,Title="Iphone 15",Price=50000});
+            urunler.Add(new Product(){Id=3,Title="Iphone 16",Price=60000});
+            
+            urunler.Insert(urunler.Count, new Product() {Id=4,Title="Deneme",Price=70000});
+            foreach(var urun in urunler){
+                Console.WriteLine(urun.Title + " " + urun.Price);
+            }
+
+            urunler.RemoveAt(2);
+            urunler.Remove(urunler[0]);
+
+        }
+    }
+
+    class Product{
+        public int Id { get; set;}
+        public string Title { get; set;}
+        public int Price { get; set;}
+    }
+}
