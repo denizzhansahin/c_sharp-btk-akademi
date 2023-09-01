b325c60524056de73856ff30ceb4b8c69c474c18
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 16:58:40 +0300

    Add files via upload

---
+++ 10-OOP/AccesModifiers/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MetodUygulama{
+    class Program{
+        static void Main(string[] args){
+        
+
+            var soru5 = new Soru(5,"Soru Metin",new string[1]{"Python"},"Python");
+            //var aaaa = soru5.cevapkontrol;//sadece bu çıktı
+            Console.WriteLine(soru5.cevapkontrol("html"));
+
+            Console.WriteLine(soru5.SoruYazdir());
+            Console.WriteLine(soru5.cevapkontrol("html"));
+
+
+        }
+
+           
+
+    }
+
+    class Soru{
+        public int SoruId { get; set; }
+        //bunun yerine aşağıdakine bak
+        public Soru(){
+            Console.WriteLine("Soru nesnesi oluşturuldu.");
+            this.SoruId = (new Random()).Next(11111,99999);
+        }
+
+        public Soru(int soruId)
+        {
+            this.SoruId=soruId;
+        }
+
+
+        public Soru(int soruId, string soruMetni, string[] secenekler, string cevap)
+        {
+            this.SoruId = SoruId;
+            this.SoruMetni=soruMetni;
+            this.Secenekler=secenekler;
+            this.Cevap=cevap;
+        }
+
+        private string SoruMetni {get;set;}
+        private string[] Secenekler{get;set;}
+        private string Cevap{get;set;}
+
+        public bool cevapkontrol(string cevap){
+            return this.Cevap.ToLower() == cevap.ToLower();
+        }
+
+
+        public string SoruYazdir(){
+            string soru = "";
+            soru += this.SoruMetni + "\n";
+            foreach(var secenek in this.Secenekler){
+                soru+=secenek+"\n";
+            }
+            return soru;
+        }
+
+    }
+}
---
+++ 10-OOP/Class-Uygulama/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp {
+    class Program{
+        static void Main(string[] args){
+            System.Console.WriteLine("Merhaba BTK");
+            Console.WriteLine("Merhaba BTK 2");
+
+            //Directory.CreateDirectory("temp");
+
+            Ogrenci ogr1 = new Ogrenci();
+            ogr1.OgrenciNo = "100";
+            ogr1.AdSoyad = "Denizhan ŞAHİN";
+            ogr1.Sube = "Universite";
+
+            Ogrenci ogr2 = new Ogrenci();
+            ogr2.OgrenciNo = "101";
+            ogr2.AdSoyad = "Deniz";
+            ogr2.Sube ="Lise";
+
+            Console.WriteLine($"{ogr1.AdSoyad} isimli {ogr1.OgrenciNo} ogrenci şubesi {ogr1.Sube}");
+            
+            Ogrenci[] ogrenciler = new Ogrenci[2];
+            ogrenciler[0] = ogr1;
+            ogrenciler[1] = ogr2;
+
+            foreach(var ogr in ogrenciler){
+                Console.WriteLine($"{ogr.AdSoyad} ve {ogr.Sube} ve {ogr.OgrenciNo}");
+            }
+
+        }
+    }
+
+    class Ogrenci{
+        public string OgrenciNo {get;set;}
+        public string AdSoyad {get;set;}
+        public string Sube {get;set;}
+
+    }
+}
