dd30cac9db8229b6d8d536eab2c5e5039ae64267
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 17:06:18 +0300

    Add files via upload

---
+++ 11-Collection/ArrayList/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Collection{
+    class Program{
+        static void Main(string[] args){
+            //ArrayList
+            //non-generic=>object
+            //dinamik
+
+            ArrayList liste = new ArrayList();
+            liste.Add(10);
+            liste.Add("10");
+            liste.Add("ali");
+            liste.Add(null);
+            liste.Add(true);
+
+            var liste2 = new ArrayList(){
+                5,"deniz",false,4.5,null
+            };
+
+            int[] sayilar = {10,20,30};
+            liste.AddRange(sayilar);
+
+            var eleman = (int)liste[0];
+            Console.WriteLine(eleman);
+            var isim = liste[2].ToString();
+
+            foreach(var item in liste){
+                Console.WriteLine(item);
+            }
+
+            //insert
+            liste.Insert(1,"sadık");
+            liste.InsertRange(2,liste2);
+
+            //remove
+            liste.Remove(10);
+            liste.RemoveAt(2);
+            liste.RemoveRange(2,3);
+
+            //contains, indexOf
+            Console.WriteLine(liste.Contains(100));
+            Console.WriteLine(liste.IndexOf(10));
+
+
+            foreach(var item in liste){
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ 11-Collection/Collection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Collection{
+    class Program{
+        static void Main(string[] args){
+            //Array
+            /*
+            int[] sayilar = new int[5];
+            sayilar[0] = 10;
+            Soru[] sorular = new SoruSoru[5];
+            sorular[0] = new Soru();
+            */
+
+            //Collections
+
+        }
+    }
+}
