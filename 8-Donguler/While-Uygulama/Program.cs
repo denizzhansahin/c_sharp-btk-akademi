599c2913bc0a5e7dd0202fc93452aba8ec2054d7
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-01 16:51:29 +0300

    Add files via upload

---
+++ 8-Donguler/While-Uygulama/Program.cs
@@ -0,0 +1,15 @@
+﻿var rnd = new Random();
+var tahmin = rnd.Next(1,100);
+
+var limit = 5;
+var kontrol = 0;
+
+var kullanici = 0;
+
+Console.WriteLine($"Tahmin edilmesi gereken sayı : {tahmin}");
+while(kontrol<=limit-1){
+    Console.WriteLine($"Lütfen bir sayı yazınız : Kalan hakkınız : {limit} için {limit-kontrol}");
+    kullanici = Convert.ToInt32(Console.ReadLine() ?? "0");
+    if(kullanici==tahmin){Console.WriteLine("Oyun bitti! Doğru Tahmin");break;}else{Console.WriteLine("Yeniden deneyiniz!");}
+    kontrol++;
+}
---
+++ 8-Donguler/While/Program.cs
@@ -0,0 +1,26 @@
+﻿var i = 0;
+
+while(i<10){
+    Console.WriteLine(i);
+    i++;
+}
+
+string[] isimler = {"denizhan","şahin"};
+var i1 = 0;
+while(i1<isimler.Length){
+    Console.WriteLine(isimler[i1]);
+    i1++;
+}
+
+var secim = "e";
+var sayac = 1;
+var toplam = 0;
+
+while(secim=="e"){
+    Console.WriteLine($"{sayac}. sayı : ");
+    toplam += Convert.ToInt32(Console.ReadLine() ?? "0");
+    Console.Write("devam etmek istiyor musunuz?(e/h)");
+    secim = Console.ReadLine() ?? "h";
+    sayac++;
+}
+Console.WriteLine($"{sayac} adet sayısının toplamı : {toplam}");
