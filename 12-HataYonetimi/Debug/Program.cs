75b2e197246c0c2be4abe9466f581017493aaa71
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2023-09-05 15:21:23 +0300

    Add files via upload

---
+++ 12-HataYonetimi/Debug/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Deneme{
+    class Program{
+        static void Main(string[] args){
+            var sayi1 = 10;
+            var sayi2 = 20;
+            var sonuc = Toplam(sayi1,sayi2);
+            Console.WriteLine(sonuc);
+        }
+
+        static int Toplam(int a, int b){
+            var toplam = a + b;
+            return toplam;
+        }
+    }
+}
---
+++ 12-HataYonetimi/HataAyiklama/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+namespace ConsoleApp{
+    class Program{
+        static void Main(string[] args){
+            //exception
+            //system.formatException - yanlış tip veri
+            //system.dividebyzeroexception - 0 bölme hatası
+            //system.nullreferenceException - alan ayrımama hatası
+            Console.Write("1.Sayi : ");
+            int sayi1= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2.Sayi : ");
+            int sayi2= Convert.ToInt32(Console.ReadLine());
+
+            var sonuc = sayi1 / sayi2;
+
+            /*
+            Urun p = null;
+            Console.Write(p.UrunAdi);
+            */
+            
+            //exception handling
+        }
+    }
+
+    class Urun{
+        public string UrunAdi {get;set;}
+    }
+}
